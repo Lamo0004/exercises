9b3c477d8fd25c5eb1d63a80aa78009818350d8e
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-01-31 09:03:24 +0100

    return værdi

--- sayHello/js.js
+++ sayHello/js.js
@@ -1,9 +1,29 @@
-const myName = "Laura";
-const firstName = "Martin";
-const lastName = "Jensen";
+"use strict";
 
-sayHello(lastName, firstName);
+// const myName = "Laura";
+// const firstName = "Martin";
+// const lastName = "Jensen";
 
-function sayHello(firstName) {
-  console.log(`Hello ${firstName}`);
+// sayHello(lastName, firstName);
+
+// function sayHello(firstName) {
+//   console.log(`Hello ${firstName}`);
+// }
+
+function greeting(firstname) {
+  return `Hello ${firstname}`;
 }
+
+// const result = greeting("any name");
+const txt = `Greeting is ${greeting("a name")}`;
+console.log(txt);
+
+// Eksperiment 1:
+// Den console logger 'hello laura" (navnet som står i gåseøjne)
+
+// Eksperiment 2:
+// Den console logger 'hello any name" (navnet som står i gåseøjne)
+// Den console logger 'hello any name" (navnet som står i gåseøjne)
+// Parameteret er undefined – den console logger 'hello undefined"
+// Den console logger hele strengen i konstanten
+// Fungere på samme måde – argumentet/paramenteret skrives i parateserne
