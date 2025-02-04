51ee7a953ea63a6857093a70fa0ea82f077ed3eb
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-02-04 11:44:57 +0100

    bar chart

---
+++ bar_chart/script.js
@@ -0,0 +1,24 @@
+const list = document.querySelector("ul");
+
+const barArray = [];
+
+setInterval(generateBars, 200);
+
+function generateBars() {
+  const randomTal = Math.round(Math.random() * 100);
+  barArray.push(randomTal);
+
+  const li = document.createElement("li");
+  li.style.setProperty("--height", randomTal);
+  list.appendChild(li);
+
+  console.log(barArray);
+
+  if (barArray.length >= 20) {
+    barArray.shift();
+  }
+
+  if (list.children.length >= 20) {
+    list.removeChild(list.children[0]);
+  }
+}
