b1cb15da5186aa045bb4781cd947bfd1986a7de8
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-02-04 10:24:45 +0100

    brødkrumme

--- breadcrumb-navigation/js.js
+++ breadcrumb-navigation/js.js
@@ -1,5 +1,6 @@
 "use stricht";
 
+// Opretter en konstant med vores array
 const bc = [
   { name: "Hvidevarer", link: "/hvidevarer" },
   { name: "Vaskemaskiner", link: "/hvidevarer/vaskemaskiner" },
@@ -10,10 +11,14 @@ document.querySelector("button").addEventListener("click", lavSti);
 
 function lavSti() {
   bc.forEach((element) => {
-    if (bc[bc.length - 1].name === element.name) {
-      document.querySelector("ul").innerHTML += `/&nbsp<li>${element.name}&nbsp</li>`;
+    //Looper over hvert element i arrayet
+    const sidsteElement = bc[bc.length - 1].name; //Finder navnet sidste element i arrayet
+    if (sidsteElement === element.name) {
+      //Hvis sidste element har samme navn som elementet den looper over sker dette
+      document.querySelector("ul").innerHTML += `/&nbsp<li>${element.name}&nbsp</li>`; //Udskriver elements navn i HTML
     } else {
-      document.querySelector("ul").innerHTML += `/&nbsp<li><a href="${element.link}">${element.name}&nbsp</a></li>`;
+      //Ellers sker dette
+      document.querySelector("ul").innerHTML += `/&nbsp<li><a href="${element.link}">${element.name}&nbsp</a></li>`; //Udskriver elements navn og opretter link i HTML
     }
   });
 }
