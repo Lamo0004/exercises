1fd8e5553bd39c01ecceba7d706aa2a3e65e01bb
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-02-07 15:05:34 +0100

    name parts

---
+++ name-parts/nameparts.js
@@ -0,0 +1,9 @@
+const name = "Peter Heronimous Lind";
+
+const firstName = name.substring(0, name.indexOf(" "));
+const middleName = name.substring(name.indexOf(" ") + 1, name.lastIndexOf(" "));
+const lastName = name.substring(name.lastIndexOf(" ") + 1);
+
+console.log(firstName);
+console.log(middleName);
+console.log(lastName);
