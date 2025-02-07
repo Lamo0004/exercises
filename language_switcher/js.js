af97f92c9313b0c376f46006cfde61cb22d79005
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-02-07 08:48:07 +0100

    language switcher stylet

--- language_switcher/js.js
+++ language_switcher/js.js
@@ -13,12 +13,48 @@ const texts = {
     ],
   },
 };
-const locale = "da";
 
-// const header = (document.querySelector(".header").textContent = texts[locale].text);
-// const footer = (document.querySelector(".footer").textContent = texts[locale].text);
+// Funktioner som ændre teksten manuelt – men IKKE dynamisk
+// const daArray = texts.da.texts;
+// const deArray = texts.de.texts;
 
-// Get a value
-// let letNavn = objektNavn.prop;
-// Set a value
-// objektNavn.prop = "";
+// function showDa(daArray) {
+//   daArray.forEach(({ text, location }) => {
+//     const element = document.querySelector(location);
+//     if (element) {
+//       element.textContent = text;
+//     }
+//   });
+// }
+// showDa(daArray);
+
+// function showDe(deArray) {
+//   deArray.forEach(({ text, location }) => {
+//     const element = document.querySelector(location);
+//     if (element) {
+//       element.textContent = text;
+//     }
+//   });
+// }
+// showDa(deArray);
+
+// Function som skifter teksten baseret på valgte sprog
+function switchLanguage(locale) {
+  const selectedTexts = texts[locale].texts; // Henter den korrekte tekst for det valgte sprog
+
+  selectedTexts.forEach(({ text, location }) => {
+    //Gennemgår hver tekst og dens placering på siden
+    const element = document.querySelector(location); //Finder HTML-elementet baseret på location
+    if (element) {
+      element.textContent = text; // Hvis elementet findes, opdateres dets tekstindhold
+    }
+  });
+}
+
+// Lytter efter ændringer i sprogvælgeren (dropdown-menuen)
+document.getElementById("language_switcher").addEventListener("change", (event) => {
+  switchLanguage(event.target.value); // Kalder funktionen med det valgte sprog for at opdatere teksten på siden
+});
+
+// Sætter sprogen til dansk default
+switchLanguage("da");
