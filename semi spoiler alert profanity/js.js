1daa9cdfb587bd234a3eda13715182c50e62d773
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-02-08 13:52:56 +0100

    semi spoiler alert

---
+++ semi spoiler alert profanity/js.js
@@ -0,0 +1,7 @@
+let theText = "Jeg er en tekst hvor nogle ord skal skiftes ud med nogle andre ord";
+
+// split & join
+console.log(theText.split("ord").join("ting"));
+
+// Replace all
+console.log(theText.replaceAll("ord", "ting"));
