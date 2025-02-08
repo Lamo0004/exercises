ab6bca79d2818edc6a14cb68c220b99b5101832e
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-02-08 14:04:31 +0100

    semi spoiler alert profanity 1 og 2

--- semi spoiler alert profanity/js.js
+++ semi spoiler alert profanity/js.js
@@ -1,3 +1,4 @@
+// SEMI SPOILER ALERT PROFANITY PREP 1
 let theText = "Jeg er en tekst hvor nogle ord skal skiftes ud med nogle andre ord";
 
 // split & join
@@ -5,3 +6,15 @@ console.log(theText.split("ord").join("ting"));
 
 // Replace all
 console.log(theText.replaceAll("ord", "ting"));
+
+// SEMI SPOILER ALERT PROFANITY PREP 2
+let theText2 = "Jeg er en tekst der skal findes ord i";
+
+console.log(theText2.includes("ord"));
+
+// ======================================
+const wordsToCheckFor = ["ord", "tekst"];
+console.log(wordsToCheckFor.some((word) => ["ord", "hyp"].includes(word))); //.some returnere true, fordi mindst ét af ordene matcher
+
+// ======================================
+console.log(wordsToCheckFor.some((word) => theText2.includes(word)));
