a77160bc2580aa46e8479aaedebf37ce3555f18d
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-02-10 08:57:55 +0100

    rettet cap øvelser

--- 3caPitalization/capitalization3.js
+++ 3caPitalization/capitalization3.js
@@ -1,5 +1,5 @@
 const name = "LAura";
 
-const thirdLetterCap = name.substring(0, 2).toLowerCase().concat(name[2].toUpperCase(), name.substring(3).toLowerCase());
+const thirdLetterCap = name.substring(0, 2).toLowerCase() + name[2].toUpperCase() + name.substring(3).toLowerCase();
 
 console.log(thirdLetterCap);
--- real Capitalization/capitalization.js
+++ real Capitalization/capitalization.js
@@ -1,5 +1,5 @@
 const name = "lAuRa";
 
-const firstLetterCap = name.substring(0, 1).toUpperCase().concat(name.substring(1).toLowerCase());
+const firstLetterCap = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
 
 console.log(firstLetterCap);
