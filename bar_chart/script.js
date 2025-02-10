4306a4d5eda9d0cc523cc7c05c5d0ad8851300f2
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-02-10 09:22:47 +0100

    kommentarer i bar chart

--- bar_chart/script.js
+++ bar_chart/script.js
@@ -2,11 +2,11 @@ const list = document.querySelector("ul");
 
 const barArray = [];
 
-setInterval(generateBars, 1000);
+setInterval(generateBars, 200); //Kalder funktionen hvert 1000 milisekundt
 
 function generateBars() {
   const randomTal = Math.round(Math.random() * 100);
-  barArray.push(randomTal);
+  barArray.push(randomTal); //Lægger det random tal til arrayet
 
   const li = document.createElement("li");
   li.style.setProperty("--height", randomTal);
@@ -15,10 +15,8 @@ function generateBars() {
   console.log(barArray);
 
   if (barArray.length >= 20) {
-    barArray.shift();
-  }
-
-  if (list.children.length >= 20) {
-    list.removeChild(list.children[0]);
+    //Hvis længden af arrayet er mere end 20
+    barArray.shift(); // Fjernes det første tal i arrayet
+    list.removeChild(list.firstElementChild); //fjernes første barn fra listen (barene)
   }
 }
