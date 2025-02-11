e941fb473a87c5694160bca456a2b66f61fd478b
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-02-11 10:07:53 +0100

    profanity filter

---
+++ profanity_filter/js.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const curseWords = [
+  { bad: "var", good: "const" },
+  { bad: "float", good: "grid" },
+  { bad: "marquee", good: "just don't" },
+];
+
+document.querySelector("button").addEventListener("click", switchWords);
+
+function switchWords() {
+  curseWords.forEach((word) => {
+    let sentence = document.querySelector("p"); //Finder HTML-elementet <p>
+    sentence.innerHTML = sentence.innerHTML.split(word.bad).join(`<span>${word.good}</span>`); //Tekstindhold opdateres med split og join, for at udskifte ord
+  });
+
+  document.querySelector("button").addEventListener("click", allReadyRightWords);
+  function allReadyRightWords() {
+    document.querySelector("dialog").showModal();
+  }
+}
