1e16483a2a49909fb8e3d12ad157ad5981eb186c
Laura Mølnitz <lamo0004@stud.kea.dk>
2025-02-14 12:59:50 +0100

    Opdateret version med local starage af valgt tema

--- theme_switcher/js.js
+++ theme_switcher/js.js
@@ -1,10 +1,18 @@
 "use strict";
 
-document.querySelector("#theme_switcher").addEventListener("change", changeTheme);
+const theStoredTheme = localStorage.getItem("storedTheme"); //Konstant som læser aktuelle valg af tema
+document.querySelector("body").dataset.theme = theStoredTheme; //Sætter det gemte tema når siden er loadet (åbnes)
+
+//Vis den er forskellig for null (intet er valgt tidligere)
+if (theStoredTheme !== null) {
+  document.querySelector("body").dataset.theme = theStoredTheme; //Sætter tema til det rigtige til det rigtige
+  document.querySelector("#theme_switcher").value = theStoredTheme; //Sætter select dropdown til det rigtige
+}
+document.querySelector("#theme_switcher").addEventListener("change", changeTheme); //Kalder funtion ved ændringer på select dropdown
 
 function changeTheme() {
-  const selectedTheme = document.querySelector("#theme_switcher").value;
-  console.log(selectedTheme);
+  const selectedTheme = document.querySelector("#theme_switcher").value; //Gemmer valget af tema i konstant
+  document.querySelector("body").dataset.theme = selectedTheme; //Sætter det valgte tema på body
 
-  document.querySelector("body").dataset.theme = selectedTheme;
+  localStorage.setItem("storedTheme", selectedTheme); //Gemmer aktuelle valg af tema
 }
